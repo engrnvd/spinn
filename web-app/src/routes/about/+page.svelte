a3156571c15ac96520472cffeafb7560d0c172e3
Naveed Hassan <08es34@gmail.com>
2022-08-24 15:18:01 +0500

    cleanup

--- web-app/src/routes/about/+page.svelte
+++ web-app/src/routes/about/+page.svelte
@@ -1,34 +1,56 @@
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+    <title>About</title>
+    <meta name="description" content="About this app"/>
 </svelte:head>
 
+<script>
+    import Divider from '../../attractions/divider/divider.svelte';
+    import Card from '../../attractions/card/card.svelte';
+    import Button from "../../attractions/button/Button.svelte"
+
+    let loading = false
+
+    function toggle() {
+        const isDark = document.documentElement.getAttribute('data-theme') === 'dark'
+        const theme = isDark ? 'light' : 'dark'
+        document.documentElement.setAttribute('data-theme', theme)
+    }
+</script>
+
 <div class="content">
-	<h1>About this app</h1>
+    <h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
+    <Button {loading} on:click={toggle}>
+        click me
+    </Button>
 
-	<pre>npm create svelte@latest</pre>
+    <Divider/>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+    <Card>
+        This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+        following into your command line and following the prompts:
+    </Card>
 
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
-	</p>
+    <pre>npm create svelte@latest</pre>
+
+    <Card>
+        The page you're looking at is purely static HTML, with no client-side interactivity needed.
+        Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+        the devtools network panel and reloading.
+    </Card>
+
+    <Divider/>
+
+    <Card>
+        The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
+        it with JavaScript disabled!
+    </Card>
 </div>
 
 <style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
-	}
+    .content {
+        width: 100%;
+        max-width: var(--column-width);
+        margin: var(--column-margin-top) auto 0 auto;
+    }
 </style>
