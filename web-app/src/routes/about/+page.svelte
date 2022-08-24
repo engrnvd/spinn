53b12243a91dd44ac3698d88e316cfac9cc21ef6
Naveed Hassan <08es34@gmail.com>
2022-08-24 18:48:31 +0500

    theme adjustments

--- web-app/src/routes/about/+page.svelte
+++ web-app/src/routes/about/+page.svelte
@@ -32,14 +32,6 @@
         following into your command line and following the prompts:
     </Card>
 
-    <pre>npm create svelte@latest</pre>
-
-    <Card>
-        The page you're looking at is purely static HTML, with no client-side interactivity needed.
-        Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-        the devtools network panel and reloading.
-    </Card>
-
     <Divider/>
 
     <Card>
