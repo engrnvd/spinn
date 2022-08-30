cdbbc40e26dcd0839c6aea62699613d7655a221a
Naveed Hassan <08es34@gmail.com>
2022-08-30 16:55:03 +0500

    event type

--- web-app/src/routes/sandbox/env/+page.server.ts
+++ web-app/src/routes/sandbox/env/+page.server.ts
@@ -1,7 +1,9 @@
 import type { PageServerLoad } from './$types'
 import { MY_SECRET } from '$env/static/private'
+import type { ServerLoadEvent } from '@sveltejs/kit'
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async (event: ServerLoadEvent) => {
+  console.log('event', event)
   return {
     secret: MY_SECRET
   }
