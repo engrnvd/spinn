cb9a7b225c238d37bcda6e14e2f16dcd4442a2dd
Naveed Hassan <08es34@gmail.com>
2022-08-30 14:10:38 +0500

    updated the logo

---
+++ web-app/src/lib/Logo.svelte
@@ -0,0 +1,25 @@
+<svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle class="main-circle" cx="100" cy="100" r="100" fill="#3FB15E"/>
+    <circle cx="63" cy="60" r="30" fill="#D9D9D9"/>
+    <circle cx="159" cy="67" r="20" fill="#D9D9D9"/>
+    <circle cx="140" cy="150" r="20" fill="#D9D9D9"/>
+    <circle cx="60" cy="147" r="20" fill="#D9D9D9"/>
+    <rect x="61.343" y="66.399" width="10" height="106" transform="rotate(-41.1755 61.343 66.399)" fill="#D9D9D9"/>
+    <rect x="56" y="66.9937" width="10" height="106" transform="rotate(-87.9729 56 66.9937)" fill="#D9D9D9"/>
+    <rect x="56" y="53.3797" width="10" height="106" transform="rotate(-2.17598 56 53.3797)" fill="#D9D9D9"/>
+</svg>
+
+<style lang="scss">
+    svg {
+        width: 2em;
+        height: 2em;
+
+        circle, rect {
+            fill: var(--body-background);
+        }
+
+        .main-circle {
+            fill: var(--main) !important;
+        }
+    }
+</style>
