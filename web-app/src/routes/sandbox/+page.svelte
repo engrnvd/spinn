4d6adf88982b295068b1776c99e693bdf2d8dd9e
Naveed Hassan <08es34@gmail.com>
2022-09-14 10:04:29 +0500

    basic api integration

--- web-app/src/routes/sandbox/+page.svelte
+++ web-app/src/routes/sandbox/+page.svelte
@@ -5,9 +5,16 @@
 
 <script>
     import Card from '../../attractions/card/card.svelte'
+    import { page } from '$app/stores'
 </script>
 
 <div class="content">
+    <ul>
+        {#each $page.data.users as user}
+            <li>{user.email}</li>
+        {/each}
+    </ul>
+
     <Card class="my-4">
         This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
         following into your command line and following the prompts:
--- web-app/src/routes/sandbox/+page.ts
+++ web-app/src/routes/sandbox/+page.ts
@@ -1,4 +1,20 @@
 import { browser, dev } from '$app/environment'
+import { LoadEvent, Load, json } from '@sveltejs/kit'
+
+export const load: Load = async ({ fetch }: LoadEvent) => {
+  // console.log('event', event)
+  const res = await fetch('http://localhost:3210/users', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+  const users = await res.json()
+  console.log('users', users)
+  return {
+    users
+  }
+}
 
 export const csr = browser
 
