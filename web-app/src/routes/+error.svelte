b2062c9a7425bf3ee6e8bf862a5821232c3b93a4
Naveed Hassan <08es34@gmail.com>
2022-09-14 14:20:42 +0500

    improved error page

--- web-app/src/routes/+error.svelte
+++ web-app/src/routes/+error.svelte
@@ -1,7 +1,13 @@
 <script>
     import { page } from '$app/stores'
+    import { dev } from '$app/environment'
 </script>
 
-<h1 class="text-center">{$page.status}: {$page.error.message}</h1>
+<h1 class="text-center">{$page.status}</h1>
+<h2>{$page.error.message}</h2>
+
+{#if dev}
+    <pre>{$page.error.stack}</pre>
+{/if}
 
 
