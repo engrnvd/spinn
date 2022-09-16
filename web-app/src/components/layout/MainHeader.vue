c49bc376be6fe6120043aead0de8aa88fd7a6174
Naveed Hassan <08es34@gmail.com>
2022-09-16 18:11:39 +0500

    added basic sass

--- web-app/src/components/layout/MainHeader.vue
+++ web-app/src/components/layout/MainHeader.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
 
 </script>
 
--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './components/U/u-core.scss'
 
 const app = createApp(App)
 
